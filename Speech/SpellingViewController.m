d199014b38fef191feae2284ecb5c0e5fefc8fe1
Minh Luu <minhl@Minhs-MacBook-Pro.local>
2014-05-30 14:28:10 -0700

    Installs WordNIK

--- Speech/SpellingViewController.m
+++ Speech/SpellingViewController.m
@@ -8,8 +8,11 @@
 
 #import "SpellingViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import "NIKWordApi.h"
+#import "NIKWordsApi.h"
 
 @interface SpellingViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *entryTF;
 
 @property (nonatomic, strong) NSArray *words;
 @property (nonatomic, strong) NSString *randomWord;
@@ -21,6 +24,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self.entryTF becomeFirstResponder];
+    
     // Load word list
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"latin" ofType:@"txt"];
     NSString *wordList = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
@@ -31,13 +37,28 @@
     self.randomWord = [self.words objectAtIndex:randomIndex];
     NSLog(@"%@", self.randomWord);
     
+    
+    
+    
     AVSpeechSynthesizer *synthesizer = [[AVSpeechSynthesizer alloc] init];
     AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:[NSString stringWithFormat:@"Your word is %@.", self.randomWord]];
     utterance.rate = AVSpeechUtteranceDefaultSpeechRate / 4;
     [synthesizer speakUtterance:utterance];
     
+//    NIKWordApi *wordApi = [[NIKWordApi alloc] init];
+//    [wordApi addHeader:@"983709d18722cf7fea51f0e68d50bf5d8c0470461f01c312e" forKey:@"api_key"];
+
+    // Get random word
+    NIKWordsApi *wordsApi = [[NIKWordsApi alloc] init];
+    [wordsApi addHeader:@"983709d18722cf7fea51f0e68d50bf5d8c0470461f01c312e" forKey:@"api_key"];
     
-    
+    [wordsApi getRandomWordWithCompletionBlock:nil excludePartOfSpeech:nil hasDictionaryDef:@"yes" minCorpusCount:nil maxCorpusCount:nil minDictionaryCount:nil maxDictionaryCount:nil minLength:@3 maxLength:nil completionHandler:^(NIKWordObject *word, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"Wordnik: %@", word.word);
+        }
+    }];
 }
 
 - (IBAction)repeat:(id)sender {
