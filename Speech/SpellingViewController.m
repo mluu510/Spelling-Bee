5f6cce5cee4efae162850e1b1a6018915d5a7610
Minh Luu <minhl@Minhs-MacBook-Pro.local>
2014-05-30 21:52:59 -0700

    Adds continuous play

--- Speech/SpellingViewController.m
+++ Speech/SpellingViewController.m
@@ -11,10 +11,13 @@
 #import "NIKWordApi.h"
 #import "NIKWordsApi.h"
 
-@interface SpellingViewController ()
+@interface SpellingViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *entryTF;
 @property (nonatomic, strong) NIKWordObject *word;
 @property (nonatomic, strong) NSArray *definitions;
+@property (nonatomic, strong) NIKExample *example;
+@property (nonatomic, strong) AVAudioPlayer *errorAudio;
+@property (nonatomic, strong) AVAudioPlayer *correctAudio;
 @end
 
 @implementation SpellingViewController
@@ -26,8 +29,23 @@
     [super viewDidLoad];
     
     [self.entryTF becomeFirstResponder];
+    
+    NSURL *errorURL = [[NSBundle mainBundle] URLForResource:@"error" withExtension:@"wav"];
+    self.errorAudio = [[AVAudioPlayer alloc] initWithContentsOfURL:errorURL error:nil];
+    [self.errorAudio prepareToPlay];
+    
+    NSURL *correctURL = [[NSBundle mainBundle] URLForResource:@"correct" withExtension:@"m4a"];
+    self.correctAudio = [[AVAudioPlayer alloc] initWithContentsOfURL:correctURL error:nil];
+    [self.correctAudio prepareToPlay];
 
-    // Get random word
+    [self getRandomWord];
+}
+
+- (IBAction)skip:(id)sender {
+    [self getRandomWord];
+}
+
+- (void)getRandomWord {
     NIKWordsApi *wordsApi = [[NIKWordsApi alloc] init];
     [wordsApi addHeader:kApiKey forKey:@"api_key"];
     
@@ -41,7 +59,39 @@
             [self getDefinition];
         }
     }];
+}
 
+- (void)flashTextFieldRed {
+    [self redTextField];
+    [self performSelector:@selector(whiteTextField) withObject:nil afterDelay:0.05];
+    [self performSelector:@selector(redTextField) withObject:nil afterDelay:0.10];
+    [self performSelector:@selector(whiteTextField) withObject:nil afterDelay:0.15];
+}
+
+- (void)redTextField {
+    self.entryTF.backgroundColor = [UIColor redColor];
+    self.entryTF.textColor = [UIColor whiteColor];
+}
+
+- (void)whiteTextField {
+    self.entryTF.backgroundColor = [UIColor whiteColor];
+    self.entryTF.textColor = [UIColor blackColor];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if ([[textField.text lowercaseString] isEqualToString:[self.word.word lowercaseString]]) {
+        textField.text = nil;
+        [self.correctAudio play];
+        [self.correctAudio prepareToPlay];
+        
+        [self performSelector:@selector(getRandomWord) withObject:nil afterDelay:2];
+    } else {
+        [self.errorAudio play];
+        [self.errorAudio prepareToPlay];
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+        [self flashTextFieldRed];
+    }
+    return NO;
 }
 
 - (void)getDefinition {
@@ -52,27 +102,49 @@
             NSLog(@"ERROR: %@", error.localizedDescription);
         } else {
             self.definitions = definitions;
-            for (NIKDefinition *defintion in definitions) {
-                NSLog(@"%@", [defintion asDictionary]);
-            }
+//            for (NIKDefinition *defintion in definitions) {
+//                NSLog(@"%@", [defintion asDictionary]);
+//            }
+        }
+    }];
+    [wordApi getTopExampleWithCompletionBlock:self.word.word useCanonical:nil completionHandler:^(NIKExample *example, NSError *error) {
+        if (error) {
+            NSLog(@"ERROR: %@", error.localizedDescription);
+        } else {
+            self.example = example;
+            NSLog(@"%@", [example asDictionary]);
         }
     }];
 }
 
 - (void)speakWord {
-    AVSpeechSynthesizer *synthesizer = [[AVSpeechSynthesizer alloc] init];
-    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:[NSString stringWithFormat:@"Your word is %@.", self.word.word]];
-    utterance.rate = AVSpeechUtteranceMaximumSpeechRate / 4;
-    [synthesizer speakUtterance:utterance];
+    [self speakText:[NSString stringWithFormat:@"Your word is %@.", self.word.word]];
 }
 
+- (IBAction)example:(id)sender {
+    [self speakText:self.example.text];
+}
+
+- (IBAction)partOfSpeech:(id)sender {
+    NIKDefinition *definition = self.definitions.firstObject;
+    [self speakText:definition.partOfSpeech];
+}
 
 - (IBAction)definition:(id)sender {
     NIKDefinition *definition = self.definitions.firstObject;
-    
+    [self speakText:[NSString stringWithFormat:@"Definition: %@.", definition.text]];
+}
+
+- (IBAction)repeat:(id)sender {
+    [self speakText:self.word.word];
+}
+
+#define kSpeechSlowDownRate 5
+
+- (void)speakText:(NSString *)text {
     AVSpeechSynthesizer *synthesizer = [[AVSpeechSynthesizer alloc] init];
-    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:[NSString stringWithFormat:@"Definition: %@.", definition.text]];
-    utterance.rate = AVSpeechUtteranceMaximumSpeechRate / 4;
+    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:text];
+    utterance.rate = AVSpeechUtteranceMaximumSpeechRate / kSpeechSlowDownRate;
     [synthesizer speakUtterance:utterance];
 }
 
@@ -81,13 +153,4 @@
     NSString *wordList = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
     return [wordList componentsSeparatedByString:@"\n"];
 }
-
-
-- (IBAction)repeat:(id)sender {
-    AVSpeechSynthesizer *synthesizer = [[AVSpeechSynthesizer alloc] init];
-    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:self.word.word];
-    utterance.rate = AVSpeechUtteranceDefaultSpeechRate / 4;
-    [synthesizer speakUtterance:utterance];
-}
-
 @end
